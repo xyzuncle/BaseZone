b42baa6da67971bdbcb2efddaace71773706da81
xyzuncle <w68335397@163.com>
2017-12-15 15:17:21 +0800

    增加通用接口，并测试

---
+++ kq-provider-service/src/main/java/com/kq/controller/RefactorHelloController.java
@@ -0,0 +1,29 @@
+package com.kq.controller;
+
+
+
+import com.kq.dto.User;
+import com.kq.iservice.HelloService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RefactorHelloController implements HelloService {
+    @Override
+    public String hello(@RequestParam("name") String name) {
+        return "Hello  "+name;
+    }
+
+    @Override
+    public User hello(@RequestHeader("name") String name, @RequestHeader("name") Integer age) {
+
+        return new User(name,age);
+    }
+
+    @Override
+    public String hello(@RequestBody  User user) {
+        return "Hello "+user.getName()+" age: "+user.getAge();
+    }
+}
